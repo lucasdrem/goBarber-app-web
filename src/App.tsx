01f9cd0a09556918b947740dda19c133cb6c06d8
lucasdrem <resets. 59625090+lucasdrem@users.noreply.github.com>
2020-09-23 21:54:12 -0300

    Projeto configurado com prettier, eslint e editorconfig

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+);
+
+export default App;
